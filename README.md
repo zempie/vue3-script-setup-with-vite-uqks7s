87a350f7a7ea8882c06f0ce910a6227105de69dd
zempie <88480567+zempie@users.noreply.github.com>
2023-04-26 12:54:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue3-script-setup-with-vite-uqks7s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue3-script-setup-with-vite-uqks7s)
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+import html2canvas from 'html2canvas'
+
+const editorRef = ref('')
+const imgSrc = ref()
+
+async function convertExcel() {
+  const editorContent = editorRef.value.textContent
+  if (editorContent) {
+    const divNode = document.getElementById('editor')
+
+    html2canvas(editorRef.value).then((canvas) => {
+      document.body.appendChild(canvas)
+      imgSrc.value = canvas.toDataURL('image/jpeg')
+    })
+  }
+}
+</script>
+<template>
+  <div id="app">
+    <div id="editor" contenteditable="true" ref="editorRef"></div>
+    <button @click="convertExcel">이미지 변환</button>
+
+    <div>
+      이미지 이미지 형식
+      <div><img :src="imgSrc" /></div>
+      <p>이미지 소스 : {{ imgSrc }}</p>
+      <div></div>
+      캔버스 형식
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div#editor,
+#capture {
+  padding: 16px 24px;
+  border: 1px solid #d6d6d6;
+  border-radius: 4px;
+}
+</style>
