33c636bbf643470e3f478b9e3c4c9feab84da9b2
zempie <88480567+zempie@users.noreply.github.com>
2023-04-26 14:53:01 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,33 @@ async function convertExcel() {
     const divNode = document.getElementById('editor')
 
     html2canvas(editorRef.value).then((canvas) => {
+      const cv = document.getElementsByTagName('canvas')
+      console.log(cv)
+      if (cv.length) {
+        cv.remove()
+      }
+
       document.body.appendChild(canvas)
-      imgSrc.value = canvas.toDataURL('image/jpeg')
+      imgSrc.value = canvas?.toDataURL('image/jpeg')
     })
+  }
+}
+
+function reset() {
+  imgSrc.value = ''
+  editorRef.value = ''
+  const canvasList = document.getElementsByTagName('canvas')
+  for (let i = 0; i < canvasList.length; i++) {
+    canvasList[i].remove()
   }
 }
 </script>
 <template>
   <div id="app">
+    <p>엑셀 테이블 삽입 및 원하는 데이터 삽입</p>
     <div id="editor" contenteditable="true" ref="editorRef"></div>
     <button @click="convertExcel">이미지 변환</button>
+    <button @click="reset">초기화</button>
 
     <div>
       이미지 이미지 형식
